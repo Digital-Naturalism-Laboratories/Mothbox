470a86437a84530fa13aec7356e1228a08132fa4
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2024-07-17 14:22:31 -0500

    added different interpolation

--- AI/Mothbox_YoloPredict_OBB.py
+++ AI/Mothbox_YoloPredict_OBB.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import numpy as np
 import os
 
-def crop_rect(img, rect):
+def crop_rect_old(img, rect):
     # get the parameter of the small rectangle
     center, size, angle = rect[0], rect[1], rect[2]
     center, size = tuple(map(int, center)), tuple(map(int, size))
@@ -21,6 +21,23 @@ def crop_rect(img, rect):
 
     return img_crop, img_rot
 
+def crop_rect(img, rect, interpolation=cv2.INTER_LINEAR): # cv2.INTER_LANCZOS4  cv2.INTER_LINEAR cv2.INTER_CUBIC
+    # get the parameter of the small rectangle
+    center, size, angle = rect[0], rect[1], rect[2]
+    center, size = tuple(map(int, center)), tuple(map(int, size))
+
+    # get row and col num in img
+    height, width = img.shape[0], img.shape[1]
+
+    # calculate the rotation matrix
+    M = cv2.getRotationMatrix2D(center, angle, 1)
+    # rotate the original image
+    img_rot = cv2.warpAffine(img, M, (width, height), flags=interpolation)
+
+    # now rotated rectangle becomes vertical, and we crop it
+    img_crop = cv2.getRectSubPix(img_rot, size, center)
+
+    return img_crop, img_rot
 
 if __name__ == '__main__':
 
