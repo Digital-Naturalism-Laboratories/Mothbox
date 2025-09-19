2a978308ddfb608a721eebbb1635ff46dc5a7077
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2025-09-19 14:12:20 -0500

    made idetect output better

--- AI/Mothbot/Mothbot_Detect.py
+++ AI/Mothbot/Mothbot_Detect.py
@@ -182,7 +182,7 @@ def process_jpg_files(img_files, date_folder):
         try:
             #print("Predict a new image with error catchers:", image_path)
             print("Predict where insects are on a new image :", image_path)
-            results = model.predict(source=image_path, imgsz=IMGSZ, device=DEVICE)
+            results = model.predict(source=image_path, imgsz=IMGSZ, device=DEVICE, verbose=False)
         except Exception as e:  # catch *any* error YOLO/PIL/numpy might throw
             print(f"❌ Skipping corrupt/unreadable image: {image_path} ({e})")
             print(f"Skipping {filename}: Image file is missing or empty and messed up in YOLO.")
