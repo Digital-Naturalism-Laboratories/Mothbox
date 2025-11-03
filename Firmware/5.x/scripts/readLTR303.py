aff9e32745624bc4015920b2a67c77538e65075e
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2025-11-03 00:57:45 -0500

    Update readLTR303.py

--- Firmware/5.x/scripts/readLTR303.py
+++ Firmware/5.x/scripts/readLTR303.py
@@ -4,7 +4,8 @@ from ltr303 import LTR303
 sensor = LTR303(bus_num=1)
 sensor.begin(gain=96, integration_time=400)
 
-while True:
+#while True:
+while False:
     lux, ch0, ch1 = sensor.read_lux()
     print(f"Lux: {lux:.5f} , Channels: CH0={ch0}, CH1={ch1}")
     time.sleep(1)
