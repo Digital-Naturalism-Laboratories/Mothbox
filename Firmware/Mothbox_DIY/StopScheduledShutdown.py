3c46d2ec68c6ce7e7ae677c7bcb80b9c6f7cbb65
Andrew Quitmeyer <andrew.quitmeyer@gmail.com>
2026-02-01 08:09:51 -0500

    Update file paths in StopScheduledShutdown.py

--- Firmware/Mothbox_DIY/StopScheduledShutdown.py
+++ Firmware/Mothbox_DIY/StopScheduledShutdown.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-with open("/boot/firmware/mothbox_custom/controls.txt", "r") as file:
+with open("/boot/firmware/mothbox_custom/system/controls.txt", "r") as file:
     lines = file.readlines()
 
-with open("/boot/firmware/mothbox_custom/controls.txt", "w") as file:
+with open("/boot/firmware/mothbox_custom/system/controls.txt", "w") as file:
     for line in lines:
         print(line)
         if line.startswith("shutdown_enabled="):
